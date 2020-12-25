6e9a779c170bbab34489000b235c8f7c75678573
ofrypeler <ofrypeler4@gmail.com>
2020-12-25 19:17:16 +0200

    updates in DB initialize implementation

--- spl-net/src/main/java/bgu/spl/net/impl/RegistrationSystem/Course.java
+++ spl-net/src/main/java/bgu/spl/net/impl/RegistrationSystem/Course.java
@@ -21,7 +21,29 @@ public class Course {
         return numOfMaxStudents-numOfCurrStudents>0;
     }
 
-    public LinkedList<Course> getKdamCourses(){
+    public int getNumOfCurrStudents() {
+        return numOfCurrStudents;
+    }
+
+    public int getNumOfMaxStudents() {
+        return numOfMaxStudents;
+    }
+
+    public void setNumOfCurrStudents(int numOfCurrStudents) {
+        this.numOfCurrStudents = numOfCurrStudents;
+    }
+
+    public void setNumOfMaxStudents(int numOfMaxStudents) {
+        this.numOfMaxStudents = numOfMaxStudents;
+    }
+
+    public ArrayList<Course> getKdamCourses(){
         return kdamCourses;
     }
+
+    public void setKdamCourses(ArrayList<Course> kdamCourses) {
+        this.kdamCourses = kdamCourses;
+    }
 }
+
+
