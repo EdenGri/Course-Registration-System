f4890f24cb75eb65f8a00cc89a167f0f808c6bdb
ofrypeler <ofrypeler4@gmail.com>
2020-12-28 15:45:50 +0200

    updates to DB fixes in kdam courses list

--- spl-net/src/main/java/bgu/spl/net/impl/RegistrationSystem/Database.java
+++ spl-net/src/main/java/bgu/spl/net/impl/RegistrationSystem/Database.java
@@ -16,7 +16,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * You can add private fields and methods to this class as you see fit.
  */
 public class Database {
-    private final static String coursesPath ="./Courses.txt";
+    private final static String coursesPath ="Courses.txt"; //TODO check if ok like this when running in VM & courses location
     private final ConcurrentHashMap<String, User> registeredUsers = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, User> connectedUsers = new ConcurrentHashMap<>(); //TODO check if need this?
     private final  ConcurrentHashMap<Integer, Course> courses = new ConcurrentHashMap<>();
