a49a7f2f39adb6fb0a207ffc080f33bfac51aeaa
EdenGri <eden7045@gmail.com>
2021-01-01 20:16:09 +0200

    add tests

--- spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/TPCMain.java
+++ spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/TPCMain.java
@@ -6,11 +6,11 @@ import bgu.spl.net.srv.Server;
 public class TPCMain {
     public static void main (String[] args){
         Database database =Database.getInstance();
-        //int port = Integer.parseInt(args[0]);//todo
+        int port = Integer.parseInt(args[0]);//todo
 
 
         Server.threadPerClient(
-                7777, //port//todo change to port
+                port, //port//todo change to port
                 () -> new MessagingProtocolImpl(database), //protocol factory
                 MessageEncoderDecoderImpl::new //message encoder decoder factory
         ).serve();
